97a71254ac5d2247ae91016a23ad0f8b5d8d0815
danil348 <eliseevdanil348@gmail.com>
2023-04-14 00:46:04 +0300

    added promo content controls, added styles for promo content

--- src/components/screens/Home/PromoContent/PromoContent.tsx
+++ src/components/screens/Home/PromoContent/PromoContent.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { BiBookmarkAltPlus } from "react-icons/bi";
+import { FaPlay } from "react-icons/fa";
 import { useMovie } from "../../../../hooks/useMovie";
 
 interface PromoContentProps {
@@ -19,24 +21,63 @@ const PromoContent: React.FC<PromoContentProps> = ({id}) => {
 	}, [movie])
 
 	return (
-		<section className='home-page__promoContent promoContent' style={{backgroundImage: `url(${movie?.assets.mainImage})`}} >
-			<motion.div className="promoContent__content"
-				initial={{  opacity: 1 }}
-				animate={{  opacity: 0 }}
-				transition={{delay: 3, duration: 0.55}}
-			>
-				<motion.img loading="lazy" src={movie?.assets.titleImage} alt="" className="promoContent__titleImage"
-					initial={{  y: -40, scale: 1.2 }}
-					animate={ {  y: 0 , scale: 1}}
-					transition={{ease: "circIn", duration: .55}}
-				/>
-				<motion.h1 className="promoContent__title"
-					initial={{  y: 40, scale: 1.2 }}
-					animate={ {  y: 0 , scale: 1}}
-					transition={{ease: "circIn", duration: .55}}
-				>{movie?.annotations}</motion.h1>
-			</motion.div>
-		</section>
+		<motion.section className='home-page__promoContent promoContent' style={{backgroundImage: `url(${movie?.assets.mainImage})`}} 
+			initial={{  opacity: 0 }}
+			animate={{  opacity: 1 }}
+			transition={{delay: 0.5, duration: 0.75}}
+		>
+			{movie ? 
+				<>
+					<motion.div className="promoContent__content"
+						initial={{  opacity: 1 }}
+						animate={{  opacity: 0 }}
+						transition={{delay: 3.5, duration: 0.15}}
+					>
+						<motion.img loading="lazy" src={movie?.assets.titleImage} alt="" className="promoContent__titleImage"
+							initial={{  y: -40, scale: 1.2 }}
+							animate={ {  y: 0 , scale: 1}}
+							transition={{ease: "circIn", duration: .55,delay: 0.8}}
+						/>
+						<motion.h1 className="promoContent__title"
+							initial={{  y: 40, scale: 1.2 }}
+							animate={ {  y: 0 , scale: 1}}
+							transition={{ease: "circIn", duration: .55,delay: 0.8}}
+						>{movie?.annotations}</motion.h1>
+					</motion.div>
+
+					<div className="promoContent__bottom promoContent-bottom">
+						<div className="promoContent-bottom__image">
+							<motion.img loading="lazy" src={movie?.assets.titleImage} alt=""
+								initial={{  y: -20, opacity: 0 }}
+								animate={ {  y: 0, opacity: 1 }}
+								transition={{ease: "easeIn", duration: .55,delay: 3.5}}
+							/>
+						</div>
+						<motion.div className="promoContent-bottom__actions promoContent-actions"
+							initial={{  y: 20, opacity: 0 }}
+							animate={ {  y: 0, opacity: 1 }}
+							transition={{ease: "easeIn", duration: .55,delay: 1.5}}
+						>
+							<div className="promoContent-actions__left">
+								<button className="promoContent-bottom__button promoContent-bottom__buttonOrange"><FaPlay/>Смотреть фильм</button>
+								<button className="promoContent-bottom__button promoContent-bottom__buttonDark">О фильме</button>
+								<button className="promoContent-bottom__button promoContent-bottom__buttonDark promoContent-bottom__buttonCircle"><BiBookmarkAltPlus size={25}/></button>
+							</div>
+							<motion.div className="promoContent-actions__right"
+								initial={{  y: 20, opacity: 0 }}
+								animate={ {  y: 0, opacity: 1 }}
+								transition={{ease: "easeIn", duration: .55,delay: 2.8}}
+							>
+								<button className="promoContent-bottom__button promoContent-bottom__buttonDark promoContent-bottom__buttonCircle"><BiBookmarkAltPlus size={25}/></button>
+								<button className="promoContent-bottom__button promoContent-bottom__buttonDark promoContent-bottom__buttonCircle"><BiBookmarkAltPlus size={25}/></button>
+							</motion.div>
+						</motion.div>
+					</div>
+				</>
+				:
+				null
+			}
+		</motion.section>
 	)
 }
 
