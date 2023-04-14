0558c257487f94cd0fdf2ef7527800495a5ac913
данил елисеев <eliseevdanil348@gmail.com>
2023-04-14 10:06:53 +0300

    work on page 404 is finished

--- src/components/screens/Page404/Page404.tsx
+++ src/components/screens/Page404/Page404.tsx
@@ -1,8 +1,24 @@
-import styles from "./Page404.module.scss";
+import { useNavigate } from "react-router-dom";
+import notFound from "../../../assets/notFound.mp4";
 
 const Page404 = () => {
+	const navigate = useNavigate()
+
 	return (
-		<div className={styles.Page404} >Page404</div>
+		<div className="notFound-page">
+			<div className="notFound-page__content">
+				<div className="notFound-page__text">
+					<h1 className="notFound-page__title">404. Страница не найдена</h1>
+					<div className="notFound-page__info">Возможно, она была перемещена, или вы просто неверно указали адрес страницы</div>
+					<div className="notFound-page__actions">
+						<button className="notFound-page__homeButton" onClick={() => navigate("/")}>На главную</button>
+					</div>
+				</div>
+				<div className="notFound-page__gif">
+					<video src={notFound} autoPlay loop muted></video>
+				</div>
+			</div>
+		</div>
 	)
 }
 
